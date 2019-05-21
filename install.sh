3271f1a6693a7a76f240b40f9cdb9ac4f8842fb6
sww1235 <sww1235@gmail.com>
2019-05-21 10:19:49 +0000

    starting on install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+# read command line arguments to determine which parts to install, or all or help
+
+if [[$# < 1 ]];
+then 
+	echo "Enter which portion of the dotfiles to install or all to install all the dotfiles";
+fi
+
+
+
+
+
+
