d475e2cb27e397df8fb98b31639b98932f3d817e
sww1235 <sww1235@gmail.com>
2019-05-22 22:12:31 +0200

    adding argument processing

--- install.sh
+++ install.sh
@@ -1,13 +1,33 @@
 #! /bin/sh
 # read command line arguments to determine which parts to install, or all or help
 
-if [[$# < 1 ]];
-then 
-	echo "Enter which portion of the dotfiles to install or all to install all the dotfiles";
-fi
+INSTALL_VIM=0
+INSTALL_BASH=0
 
+# loop through arguments and process them
+# ideas from https://pretzelhands.com/posts/command-line-flags
 
-
-
+for arg in "$@" # all arguments except $0
+do
+	case $arg in
+	-h|--help)
+		echo "type -l or --list for list of parts to install or \"all\" to install all parts"
+		shift # remove -h or --help from processing
+		exit(0)
+		;;
+	vim)
+		INSTALL_VIM=1
+		shift # remove vim from processing
+		;;
+	bash)
+		INSTALL_BASH=1
+		shift # remove bash from processing
+		;;
+	*)
+		echo "Argument $1 is not recognized"
+		shift
+		;;
+	esac
+done
 
 
